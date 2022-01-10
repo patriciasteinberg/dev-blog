803537fa32a0d6cafb48be6a3729dcc68b49bee0
Patricia Steinberg <50302759+patriciasteinberg@users.noreply.github.com>
2022-01-09 23:42:05 -0500

    001: routing

---
+++ pages/[username]/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const UserProfilePage = () => {
+    return (
+        <div>
+            <h1>Profile page</h1>
+        </div>
+    )
+}
+
+export default UserProfilePage;
