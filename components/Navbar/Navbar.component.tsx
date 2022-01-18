bb0f2f54b990c4ca1eb258c301378de5c63616a2
Patricia Steinberg <50302759+patriciasteinberg@users.noreply.github.com>
2022-01-18 13:45:24 -0500

    002: loader, toaster

--- components/Navbar/Navbar.component.tsx
+++ components/Navbar/Navbar.component.tsx
@@ -1,7 +1,49 @@
-import React from "react";
+import Link from 'next/link';
 
-const Navbar = () => {
-  return <div></div>;
-};
+interface IUser {
+  photoURL: string;
+}
+
+interface INavBar {
+  user?: IUser,
+  username?: IUser
+}
+
+const Navbar = ({ user, username }: INavBar) => {
+  return (
+    <nav className="navbar">
+      <ul>
+        <li>
+          <Link href="/">
+            <button className="btn-logo">FEED</button>
+          </Link>
+        </li>
+        {username && (
+          <>
+            <li className="push-left">
+              <Link href="/admin">
+                <button className="btn-blue">Write Posts</button>
+              </Link>
+            </li>
+            {user && (
+              <li>
+                <Link href={`/${username}`}>
+                  <img src={user.photoURL} />
+                </Link>
+              </li>
+            )}
+          </>
+        )}
+        {!username && (
+          <li>
+            <Link href="/enter">
+              <button className="btn-blue">Log in</button>
+            </Link>
+          </li>
+        )}
+      </ul>
+    </nav>
+  );
+}
 
 export default Navbar;
